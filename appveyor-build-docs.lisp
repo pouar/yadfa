41467f47d4799becdf0a6e80d8596eec58fbc01a
Pouar <pouar@pouar.net>
2018-09-04 13:18:35 -0500

    build docs

---
+++ appveyor-build-docs.lisp
@@ -0,0 +1,14 @@
+;; -*- mode: common-lisp; -*-
+#-quicklisp
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                          (user-homedir-pathname))))
+    (when (probe-file quicklisp-init)
+        (load quicklisp-init)))
+#+(and gmp sbcl) (require 'sb-gmp)
+#+(and sbcl gmp) (sb-gmp:install-gmp-funs)
+(pushnew :yadfa/mods *features*)
+(pushnew :yadfa/docs *features*)
+(handler-bind ((error #'(lambda (c) (continue))))
+    (ql:quickload :yadfa))
+(in-package :yadfa)
+(yadfa::main)
