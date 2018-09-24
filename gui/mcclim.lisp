c3b530af3ecf02902ba60c4a788010d07b949dad
Pouar <pouar@pouar.net>
2018-09-24 13:48:46 -0500

    bug in McCLIM got fixed, No longer need this workaround

--- gui/mcclim.lisp
+++ gui/mcclim.lisp
@@ -1,25 +1,17 @@
 (in-package :clim-listener)
 ;;;; because it was quicker and easier than trying to write one of these myself from scratch
 
-(clim:define-command-table clim-listener::yadfa-commands)
-(clim:define-command-table clim-listener::listener :inherit-from (clim-listener::application-commands
-                                                                clim-listener::lisp-commands
-                                                                clim-listener::asdf-commands
-                                                                clim-listener::filesystem-commands
-                                                                clim-listener::show-commands
-                                                                clim-listener::yadfa-commands)
-    :menu (("Listener"   :menu clim-listener::application-commands)
-              ("Lisp"       :menu clim-listener::lisp-commands)
-              ("Filesystem" :menu clim-listener::filesystem-commands)
-              ("Show"       :menu clim-listener::show-commands)
-              ("Yadfa" :menu clim-listener::yadfa-commands)))
-;(clim-listener::define-listener-command (yadfa-command :menu ("Yadfa" . (:after "Yadfa"))) ((test 'string :prompt "test2")) (print test))
-;(clim-listener::add-menu-item-to-command-table (clim-listener::find-command-table 'yadfa-commands) "Yadfa" 'string "2")
-(clim:define-command (yadfa-set-player :command-table clim-listener::yadfa-commands :menu "Set Player")
+(clim:define-command-table yadfa-commands)
+(clim:define-command (yadfa-set-player :command-table yadfa-commands :menu "Set Player")
     ((name 'string
-         :prompt "Name")
+         :prompt "Name"
+         :default (yadfa:name-of (yadfa:player-of yadfa:*game*)))
         (gender 'boolean
-            :prompt "Is Male")
+            :prompt "Is Male"
+            :default (yadfa:malep (yadfa:player-of yadfa:*game*)))
         (species 'string
-            :prompt "Species"))
+            :prompt "Species"
+            :default (yadfa:species-of (yadfa:player-of yadfa:*game*))))
     (yadfa/bin::set-player name gender species))
+(clim:add-menu-item-to-command-table (clim:find-command-table 'listener) "Yadfa" :menu (clim:find-command-table 'yadfa-commands))
+(pushnew (clim:find-command-table 'yadfa-commands) (clim:command-table-inherit-from (clim:find-command-table 'listener)))
