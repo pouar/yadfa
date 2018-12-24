e8e5be2e8a05a4d24c1262eb8f790cc91900b068
Pouar <pouar@pouar.net>
2018-12-24 12:56:26 -0600

    namespacing

--- core/mcclim.lisp
+++ core/mcclim.lisp
@@ -1,6 +1,6 @@
 (in-package :clim-listener)
 ;;;; because it was quicker and easier than trying to write one of these myself from scratch
-(macro-level
+(macro-level:macro-level
     `(setf *default-text-style*
          (make-text-style ,@(if (member :win32 *features*) '(:fix :roman) '("DejaVu Sans Mono" "Book")) :normal)))
 
