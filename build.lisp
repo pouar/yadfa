7c7a88d4183995a828e2b8121441dfdaf6aa1a5c
Pouar <pouar@pouar.net>
2019-01-13 16:09:20 -0600

    better workaround

--- build.lisp
+++ build.lisp
@@ -1,5 +1,71 @@
 ;; -*- mode: common-lisp; -*-
-#+sbcl (sb-ext:restrict-compiler-policy 'debug 2)
+
+;;; https://bugs.launchpad.net/sbcl/+bug/1811623
+#+sbcl
+(in-package :sb-c)
+#+sbcl
+(defun convert-mv-bind-to-let (call)
+    (declare (type mv-combination call))
+    (let* ((args (basic-combination-args call))
+              (use (lvar-uses (first args))))
+        (when (and (singleton-p args)
+                  (combination-p use)
+                  (eq (lvar-fun-name (combination-fun use))
+                      'values))
+            (setf (lvar-reoptimize (car args)) nil)
+            (let* ((fun (combination-lambda call))
+                      (vars (lambda-vars fun))
+                      (vals (combination-args use))
+                      (nvars (length vars))
+                      (nvals (length vals)))
+                (cond ((> nvals nvars)
+                          (mapc #'flush-dest (subseq vals nvars))
+                          (setq vals (subseq vals 0 nvars)))
+                    ((< nvals nvars)
+                        (with-ir1-environment-from-node use
+                            (let ((node-prev (node-prev use)))
+                                (setf (node-prev use) nil)
+                                (setf (ctran-next node-prev) nil)
+                                (collect ((res vals))
+                                    (loop for count below (- nvars nvals)
+                                        for prev = node-prev then ctran
+                                        for ctran = (make-ctran)
+                                        and lvar = (make-lvar use)
+                                        do (reference-constant prev ctran lvar nil)
+                                        (res lvar)
+                                        finally (link-node-to-previous-ctran
+                                                    use ctran))
+                                    (setq vals (res)))))))
+                (setf (combination-args use) vals)
+                (flush-dest (combination-fun use))
+                (let ((fun-lvar (basic-combination-fun call)))
+                    (setf (lvar-dest fun-lvar) use)
+                    (setf (combination-fun use) fun-lvar)
+                    (flush-lvar-externally-checkable-type fun-lvar))
+                (setf (combination-kind use) :local)
+                (setf (functional-kind fun) :let)
+                (flush-dest (first (basic-combination-args call)))
+                (unlink-node call)
+                (when vals
+                    (reoptimize-lvar (first vals)))
+                ;; Propagate derived types from the VALUES call to its args:
+                ;; transforms can leave the VALUES call with a better type
+                ;; than its args have, so make sure not to throw that away.
+                (let ((types (values-type-types (node-derived-type use))))
+                    (dolist (val vals)
+                        (when types
+                            (let ((type (pop types)))
+                                (assert-lvar-type val type **zero-typecheck-policy**)))))
+                ;; Propagate declared types of MV-BIND variables.
+                (propagate-to-args use fun)
+                (reoptimize-call use))
+            t)))
+#+sbcl
+(compile 'convert-mv-bind-to-let)
+#+sbcl
+(in-package :cl-user)
+
+
 #-quicklisp
 (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
                           (user-homedir-pathname))))
