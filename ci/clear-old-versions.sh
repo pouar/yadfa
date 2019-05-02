08e58f57bb7866d8b7a82d80737039c61e4472d1
Pouar <pouar@pouar.net>
2019-05-02 10:05:16 -0500

    silence curl when using it

--- ci/clear-old-versions.sh
+++ ci/clear-old-versions.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-for i in $(curl -L -u "pouar:$bintrayapikey" https://api.bintray.com/packages/pouar/yadfa-chocolatey/yadfa|jq -r .versions[1:][]);do curl -X DELETE -L -u "pouar:$bintrayapikey" "https://api.bintray.com/packages/pouar/yadfa-chocolatey/yadfa/versions/$i";done
+for i in $(curl -s -L -u "pouar:$bintrayapikey" https://api.bintray.com/packages/pouar/yadfa-chocolatey/yadfa|jq -r .versions[1:][]);do curl -s -X DELETE -L -u "pouar:$bintrayapikey" "https://api.bintray.com/packages/pouar/yadfa-chocolatey/yadfa/versions/$i";done
