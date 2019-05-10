1867158ce515a3faa373722e086c849882c00b22
Pouar <pouar@pouar.net>
2019-05-10 17:55:49 -0500

    only on master

--- ci/clear-old-versions.sh
+++ ci/clear-old-versions.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-for i in $(curl -s -L -u "pouar:$bintrayapikey" https://api.bintray.com/packages/pouar/yadfa-chocolatey/yadfa|jq -r .versions[1:][]);do curl -s -X DELETE -L -u "pouar:$bintrayapikey" "https://api.bintray.com/packages/pouar/yadfa-chocolatey/yadfa/versions/$i";done
+if [ "$APPVEYOR_REPO_BRANCH" = "master" ];then
+    for i in $(curl -s -L -u "pouar:$bintrayapikey" https://api.bintray.com/packages/pouar/yadfa-chocolatey/yadfa|jq -r .versions[1:][]);do
+        curl -s -X DELETE -L -u "pouar:$bintrayapikey" "https://api.bintray.com/packages/pouar/yadfa-chocolatey/yadfa/versions/$i"
+    done
+fi
