dce17d92f4d8add83c9f46bc5e49341bfddfcca7
Pouar <pouar@pouar.net>
2019-07-14 14:16:22 -0500

    don't put all this in clim-global-command-table

--- core/mcclim.lisp
+++ core/mcclim.lisp
@@ -1,5 +1,6 @@
 (in-package :yadfa)
 (clim:define-command-table yadfa-menu-commands)
+(clim:define-command-table yadfa-presentation-commands)
 (clim:define-command (yadfa-set-eol-action :command-table yadfa-menu-commands :menu "Set EOL Action")
     ((keyword '(member :scroll :allow :wrap :wrap*)
               :prompt "Keyword"))
@@ -11,19 +12,8 @@
     (clim:find-menu-item "Yadfa" (clim:find-command-table 'clim-listener::listener) :errorp nil)
   (clim:add-menu-item-to-command-table (clim:find-command-table 'clim-listener::listener) "Yadfa" :menu (clim:find-command-table 'yadfa-menu-commands)))
 (pushnew (clim:find-command-table 'yadfa-menu-commands) (clim:command-table-inherit-from (clim:find-command-table 'clim-listener::listener)))
-(clim:define-presentation-action zone-presentation-menu
-    (zone nil clim:global-command-table
-     :documentation "Menu"
-     :menu nil
-     :gesture :menu)
-    (object presentation frame window x y)
-  (declare (ignore object))
-  (clim:call-presentation-menu presentation clim:*input-context*
-                               frame window x y
-                               :for-menu t
-                               :label (format nil "Operation on ~A"
-                                              (clim:presentation-type presentation))))
-(clim:define-command (com-yadfa-move :command-table clim:global-command-table :menu t :name "Move Here")
+(pushnew (clim:find-command-table 'yadfa-presentation-commands) (clim:command-table-inherit-from (clim:find-command-table 'clim-listener::listener)))
+(clim:define-command (com-yadfa-move :command-table yadfa-presentation-commands :menu t :name "Move Here")
     ((zone zone))
   (block nil
     (apply #'yadfa-world:move
@@ -102,22 +92,11 @@
              (t
               (format t "You're either already on that zone or you tried specifying a path that involves turning (which this interface can't do because Pouar sucks at writing code that generates paths)~%")
               (return))))))
-(clim:define-command (com-yadfa-describe-zone :command-table clim:global-command-table :menu t :name "Print Zone Description")
+(clim:define-command (com-yadfa-describe-zone :command-table yadfa-presentation-commands :menu t :name "Print Zone Description")
     ((zone zone))
   (yadfa-bin:lst :describe-zone zone))
-(clim:define-presentation-to-command-translator com-describe-object-translator
-    (yadfa-class climi::com-describe clim:global-command-table
-     :gesture :describe
-     :documentation "Describe"
-     :pointer-documentation "Describe"
-     :tester ((object presentation)
-              (declare (ignore object))
-              (not (eq presentation climi::*null-presentation*)))
-     :menu nil)
-    (object)
-  (list object))
 (clim:define-presentation-to-command-translator com-yadfa-move-translator
-    (zone com-yadfa-move clim:global-command-table
+    (zone com-yadfa-move yadfa-presentation-commands
      :documentation "Move"
      :pointer-documentation "Move Here"
      :gesture nil
@@ -125,7 +104,7 @@
     (object)
   (list object))
 (clim:define-presentation-to-command-translator com-yadfa-describe-zone-translator
-    (zone com-yadfa-describe-zone clim:global-command-table
+    (zone com-yadfa-describe-zone yadfa-presentation-commands
      :documentation "Print Zone Description"
      :pointer-documentation "Print Zone Description"
      :gesture nil
