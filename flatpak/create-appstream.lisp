5610d1a83767e6648fa7097325f08b3af18745b3
Pouar <pouar@pouar.net>
2019-08-01 12:14:46 -0500

    don't load :yadfa when generating appstream metadata

--- flatpak/create-appstream.lisp
+++ flatpak/create-appstream.lisp
@@ -1,12 +1,11 @@
-(ql:quickload :yadfa)
 (ql:quickload :plump-sexp)
 (with-open-file (r (uiop:merge-pathnames*
-                    (make-pathname :name "net.pouar.yadfa.appdata.xml" :type "lisp")
-                    (translate-logical-pathname "yadfa:home;flatpak;"))
+                    (make-pathname :directory '(:relative "flatpak") :name "net.pouar.yadfa.appdata.xml" :type "lisp")
+                    (asdf:component-pathname (asdf:find-system "yadfa")))
                    :direction :input)
   (with-open-file (s (uiop:merge-pathnames*
-                      (make-pathname :name "net.pouar.yadfa.appdata" :type "xml")
-                      (translate-logical-pathname "yadfa:home;flatpak;"))
+                      (make-pathname :directory '(:relative "flatpak") :name "net.pouar.yadfa.appdata" :type "xml")
+                      (asdf:component-pathname (asdf:find-system "yadfa")))
                      :direction :output
                      :if-exists :supersede)
     (plump:serialize (plump-sexp:parse (read r)) s)))
