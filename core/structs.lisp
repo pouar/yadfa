cc82209bca7faa569f11f7d8849cce853d3c483a
Pouar <pouar@pouar.net>
2019-10-02 16:45:35 -0500

    inline these functions
    The ANSI standard says redefining structures is undefined behavior, so
    inlining these shouldn't be a problem

--- core/structs.lisp
+++ core/structs.lisp
@@ -1,5 +1,6 @@
 ;;;; -*- mode: Common-Lisp; sly-buffer-package: "yadfa"; coding: utf-8-unix; -*-
 (in-package :yadfa)
+(declaim (inline make-event event-id event-lambda event-predicate event-repeatable event-major event-major-depends event-optional event-finished-depends event-attributes make-action action-documentation action-attributes action-lambda copy-event copy-action event-p action-p))
 (defstruct event
   "An event in the game"
   (id nil
