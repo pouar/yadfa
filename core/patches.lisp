91cfd6054d81624e3bdee19781c459bd573f516e
Pouar <pouar@pouar.net>
2020-03-31 16:46:07 -0500

    since this is sbcl specific, let's not try to patch this unless sbcl is actually used

--- core/patches.lisp
+++ core/patches.lisp
@@ -313,25 +313,15 @@
              ((eq f :bold) '((:style . "Bold")))
              ((eq f :italic) '((:style . "Italic")))
              (t nil))))
-(in-package :asdf/output-translations)
+#+sbcl (in-package :asdf/output-translations)
 
 ;;; https://gitlab.common-lisp.net/asdf/asdf/issues/28
+#+sbcl
 (defun wrapping-output-translations ()
     `(:output-translations
-    ;; Some implementations have precompiled ASDF systems,
-    ;; so we must disable translations for implementation paths.
-      #+(or clasp #|clozure|# ecl mkcl)
       ,@(let ((h (resolve-symlinks* (lisp-implementation-directory))))
-          (when h `(((,h ,*wild-path*) ()))))
-      #+sbcl ,@(let ((h (resolve-symlinks* (lisp-implementation-directory))))
                  (when (and h (not (eq uiop:*image-dumped-p* :executable))) `(((,h ,*wild-path*) ()))))
-      #+mkcl (,(translate-logical-pathname "CONTRIB:") ())
-      ;; All-import, here is where we want user stuff to be:
       :inherit-configuration
-      ;; These are for convenience, and can be overridden by the user:
-      #+abcl (#p"/___jar___file___root___/**/*.*" (:user-cache #p"**/*.*"))
-      #+abcl (#p"jar:file:/**/*.jar!/**/*.*" (:function translate-jar-pathname))
-      ;; We enable the user cache by default, and here is the place we do:
       :enable-user-cache))
 (in-package :yadfa)
 (define-condition uwu (simple-error) ()
