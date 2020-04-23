4568ff010640df1773c159fe38e20466f2869e9f
Pouar <pouar@pouar.net>
2020-04-23 14:33:15 -0500

    update example mod

--- mods/example/package.lisp
+++ mods/example/package.lisp
@@ -1,3 +1,5 @@
-(in-package #:cl-user)
-(uiop:define-package #:yadfa.furaffinity.pouar.myawesomemod (:use #:cl #:yadfa-util #:yadfa)
-  (:export :celebrate))
+(in-package :cl-user)
+(uiop:define-package :yadfa.furaffinity.pouar.myawesomemod
+  (:use :cl)
+  (:export :celebrate)
+  (:local-nicknames (:y :yadfa) (:yz :yadfa-zones)))
